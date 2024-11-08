ecf04949a6dba1c6159242f2bc086158b701a934
ivanmiros1 <ivan.malynyak@sleepnumber.com>
2024-11-08 20:04:32 +0100

    Add print_arr() function

--- ju_1.cpp
+++ ju_1.cpp
@@ -1,23 +1,35 @@
 // ...
 #include <iostream> 
-#include <cmath>
-#include <ctime>
-#include <cstdlib>
 #include <cstring>
-#include <sstream>
 using namespace std;
 
 
- int main(){
-   int M; cin >> M;
-
-   int A[M][M]; memset(A, 0, sizeof(A));
-
-A[1][0] = 2;A[1][1] = 1;A[1][2] = 1;A[1][3] = 1;A[1][4] = 0;A[1][5] =4 ;A[1][6] = 0;A[1][7] = 4;A[1][8] =2 ;A[1][9] = 0;A[1][10] = 2;A[1][11] =0 ;
-   for (int i=0 ; i<M ; i++){
-            for (int j=0 ; j<M ; j++ ) cout << A[i][j] << " ";
+// print square 2D array
+void print_arr(int ** A, int size) {
+    for (int i=0 ; i<size ; i++){
+        for (int j=0 ; j<size ; j++ ) cout << A[i][j] << " ";
         cout << endl;
-        }
+    }
+
+}
+
+
+
+
+ int main(){
+    int M; cin >> M;
+
+    // Array of pointers - columns
+    int **A = new int* [M];
+    // Add arrays of rows + init to 0
+    for(int i = 0; i < M; i++) {
+        A[i] = new int[M];
+        memset(A[i], 0, sizeof(int)*M);
+    }
+
+    A[1][0] = 2;A[1][1] = 1;A[1][2] = 1;A[1][3] = 1;A[1][4] = 0;A[1][5] =4 ;A[1][6] = 0;A[1][7] = 4;A[1][8] =2 ;A[1][9] = 0;A[1][10] = 2;A[1][11] =0 ;
+
+    print_arr(A,M);
 
     char command;
     while(cin >> command && command != 'K' ){ 
@@ -65,10 +77,7 @@ A[1][0] = 2;A[1][1] = 1;A[1][2] = 1;A[1][3] = 1;A[1][4] = 0;A[1][5] =4 ;A[1][6] 
          
 
 
-        for (int i=0 ; i<M ; i++){
-            for (int j=0 ; j<M ; j++ ) cout << A[i][j] << " ";
-        cout << endl;
-        }
+        print_arr(A,M);
     }
 
 }
